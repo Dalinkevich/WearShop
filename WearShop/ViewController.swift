85de7da17a1059d28422cdb202dc6fcb4dc702e0
Roman <Dalinkevich>
2020-07-15 14:00:02 +0300

    Initial Commit

---
+++ WearShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WearShop
+//
+//  Created by Роман далинкевич on 15.07.2020.
+//  Copyright © 2020 Роман далинкевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
